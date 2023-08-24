c36dc1023f9dcd5a8a7c37ec7adaf15eadf83b20
itsAki02 <akanksha021817@gmail.com>
2023-08-24 23:25:11 +0530

    pattern 8

--- Patterns/Patttern8.java
+++ Patterns/Patttern8.java
@@ -2,10 +2,19 @@ package Patterns;
 
 public class Patttern8 {
     public static void main(String[] args) {
-        
+        pattern8(4);
     }
 
     static void pattern8(int n){
-        
+        int originalN = n;
+        n = 2 * n;
+        for(int row = 0; row <= n; row++){
+            for(int col = 0; col <= n; col++){
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n-row, n-col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+
+        }
     }
 }
