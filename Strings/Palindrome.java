4a7e15c90aa1ec5d00fc85078e286239e2c0e365
itsAki02 <akanksha021817@gmail.com>
2023-08-26 18:50:37 +0530

    recursion

--- Strings/Palindrome.java
+++ Strings/Palindrome.java
@@ -2,12 +2,12 @@ package Strings;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String str = "abccba";
+        String str = "abc";
         System.out.println(isPalindrome(str));
     }
-    static boolean isPalindrome(String str){
+    static String isPalindrome(String str){
         if(str == null || str.length()== 0){
-            return true;
+            return "String is palindrome";
         }
         str = str.toLowerCase();
         for(int i = 0; i <= str.length()/2; i++){
@@ -15,10 +15,10 @@ public class Palindrome {
             char end = str.charAt(str.length()- 1 -i);
 
             if(start != end){
-                return false;
+                return "String is not palindrome";
             }
 
         }
-        return true;
+        return "String is palindrome";
     }
 }
