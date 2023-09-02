01231fee8c25e41da4db19f349d7e2712988103e
itsAki02 <akanksha021817@gmail.com>
2023-09-02 20:08:19 +0530

    Doubly Linked list

--- Liked_list/LL.java
+++ Liked_list/LL.java
@@ -1,18 +1,28 @@
 public class LL {
     public static void main(String[] args) {
-    LinkedList1 list = new LinkedList1();
+    // LinkedList1 list = new LinkedList1();
+    // list.insertFirst(3);
+    // list.insertFirst(8);
+    // list.insertFirst(17); 
+    // list.insertLast(99);
+    // list.insert(100, 3);
+    // list.diplay();
+    // System.out.println(list.delFirst());
+
+    // list.diplay();
+    // System.out.println(list.deleteLast());
+    // list.diplay();
+    // System.out.println(list.delete(2));
+    // list.diplay();
+
+    DLL list = new DLL();
     list.insertFirst(3);
+    list.insertFirst(2);
     list.insertFirst(8);
     list.insertFirst(17); 
     list.insertLast(99);
-    list.insert(100, 3);
-    list.diplay();
-    System.out.println(list.delFirst());
-
-    list.diplay();
-    System.out.println(list.deleteLast());
-    list.diplay();
-    System.out.println(list.delete(2));
-    list.diplay();
+    list.insert(8, 65);
+    
+    list.display();
 }
 }
