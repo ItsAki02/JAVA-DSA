01ea20e096bfbb7315ebbff931a36819d369fe2d
itsAki02 <akanksha021817@gmail.com>
2023-09-02 21:17:26 +0530

    Circular Linked List

--- Liked_list/LL.java
+++ Liked_list/LL.java
@@ -15,14 +15,24 @@ public class LL {
     // System.out.println(list.delete(2));
     // list.diplay();
 
-    DLL list = new DLL();
-    list.insertFirst(3);
-    list.insertFirst(2);
-    list.insertFirst(8);
-    list.insertFirst(17); 
-    list.insertLast(99);
-    list.insert(8, 65);
+    // DLL list = new DLL();
+    // list.insertFirst(3);
+    // list.insertFirst(2);
+    // list.insertFirst(8);
+    // list.insertFirst(17); 
+    // list.insertLast(99);
+    // list.insert(8, 65);
     
+    // list.display();
+
+    CLL list  = new CLL();
+    list.insert(23);
+    list.insert(3);
+    list.insert(19);
+    list.insert(75);
+    list.insert(21);
+    list.display();
+    list.delete(19);
     list.display();
 }
 }
